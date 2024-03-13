136f65bea156c30450f3d3d2448712c32adccc39
peapodsfinance <team@peapods.finance>
2024-03-13 14:52:19 -0400

    use base ERC20, add burnFrom

--- contracts/interfaces/IERC20Bridgeable.sol
+++ contracts/interfaces/IERC20Bridgeable.sol
@@ -6,5 +6,7 @@ import '@openzeppelin/contracts/interfaces/IERC20.sol';
 interface IERC20Bridgeable is IERC20 {
   function burn(uint256 amount) external;
 
+  function burnFrom(address wallet, uint256 amount) external;
+
   function mint(address wallet, uint256 amount) external;
 }
