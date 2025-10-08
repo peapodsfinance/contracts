5d7487e2116b2d51694654d4301cd87e7f976660
peapodsfinance <team@peapods.finance>
2025-10-08 11:21:38 -0400

    revert flash source script

--- script/DeployFlashSources.s.sol
+++ script/DeployFlashSources.s.sol
@@ -2,25 +2,21 @@
 pragma solidity ^0.8.0;
 
 import "forge-std/Script.sol";
-import "../contracts/flash/MoolahFlashSource.sol";
-import "../contracts/lvf/LeverageFactory.sol";
+import "../contracts/flash/BalancerFlashSource.sol";
 import "../contracts/lvf/LeverageManager.sol";
 
 contract DeployFlashSources is Script {
     function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
 
-        address lvf = 0x7189B95CaCCC2E5734b1592CCC8CaeAeb87573f5;
-        address levFact = 0xDea69B4ac0fB33b472E389f49F2876b98075E918;
+        address lvf = vm.envAddress("LVF");
 
-        LeverageFactory(levFact).transferContractOwnership(lvf, vm.addr(vm.envUint("PRIVATE_KEY")));
-        address balancer = address(new MoolahFlashSource(lvf, 0x8F73b65B4caAf64FBA2aF91cC5D4a2A1318E5D8C));
-        LeverageManager(payable(lvf)).setFlashSource(0x55d398326f99059fF775485246999027B3197955, balancer);
-        LeverageManager(payable(lvf)).setFlashSource(0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c, balancer);
-        LeverageManager(payable(lvf)).transferOwnership(levFact);
+        address balancer = address(new BalancerFlashSource(lvf));
+        LeverageManager(payable(lvf)).setFlashSource(vm.envAddress("ASSET1"), balancer);
+        LeverageManager(payable(lvf)).setFlashSource(vm.envAddress("ASSET2"), balancer);
 
         vm.stopBroadcast();
 
-        console.log("MoolahFlashSource deployed to:", balancer);
+        console.log("BalancerFlashSource deployed to:", balancer);
     }
 }
